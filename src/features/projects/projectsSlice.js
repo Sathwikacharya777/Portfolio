882d2f8108c720bcfa55ac662049dc67be9acd7f
Sathwik <sathwikacharyaofficial@gmail.com>
2026-02-04 11:44:27 +0530

    project added

--- src/features/projects/projectsSlice.js
+++ src/features/projects/projectsSlice.js
@@ -3,9 +3,23 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = [
   {
+    title: "Linear UI Clone",
+    category: "Frontend(Next.js)",
+    image: "/assets/linear-clone.png",
+    Builtfor: "Practicing modern SaaS UI design with smooth animations and clean layouts",
+    link: " https://linear-clone-jade.vercel.app/"
+  },
+    {
+    title: "Google Maps Place Detail UI Clone",
+    category: "Frontend(Next.js)",
+    image: "/assets/google-map.jpg",
+    Builtfor: "Replicating real-world location-based place details UI with responsive layout and clean components",
+    link: "https://google-maps-clone-uoo5.vercel.app"
+  },
+  {
     title: "Tourism Management System",
     category: "Fullstack",
-    image: "/assets/bg_4.jpg", // Ensure this file exists in /public/assets
+    image: "/assets/bg_4.jpg", 
     Builtfor: "Travel agencies, tour operators, and customers for seamless booking and management",
     link: "https://github.com/Sathwikacharya777/Tourism_Management_System"
   },
@@ -25,14 +39,14 @@ const initialState = [
   },
   {
     title: "Panto Furniture Landing",
-    category: "Frontend",
+    category: "Frontend(React.js)",
     image: "/assets/landing.jpg",
     Builtfor: "Furniture brands and businesses to showcase products and attract customers",
-    link: "https://panto-furniture-three.vercel.app"
+    link: "https://panto-furniture-three.vercel.app/"
   },
   {
     title: "E-commerce Website",
-    category: "Frontend",
+    category: "Frontend(React.js)",
     image: "/assets/bg2.webp",
     Builtfor: "Small businesses and startups for online product showcasing and sales",
     link: "https://github.com/Sathwikacharya777/Ecommerce_Frontend_React"
